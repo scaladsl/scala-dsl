c9f9a3d638751d6e47763f986e9b62662f9d574f
anahitm <anahit.mirzoyan@iunetworks.am>
2016-08-02 18:34:03 +0400

    java class generator

--- src/main/scala/org.edsl/Application.scala
+++ src/main/scala/org.edsl/Application.scala
@@ -6,26 +6,21 @@ import org.edsl.DSL._
 
 object Application {
 
-  // int, long, bool, string, date, float
-
   def main(args: Array[String]): Unit = {
 
     'foo namespace {
       'user1 struct {
         'username as 'string
         'address as 'string
-        's as 'Long
-        'af as 'int
       }
+      //!("rhyfr")
       'bar namespace {
         'user2 struct {
-          'username as 'string
-          'address as 'string
+          'username2 as 'string
+          'address_a as 'string
         }
       }
     }
-
-    // kikos_poxos => KikosPoxos, kikosPoxos, getKikosPoxos()
 
     val root = Context.current.asInstanceOf[Namespace]
     val java = new Java("/home/anahitm/dev/dsl-output")
@@ -40,27 +35,76 @@ object Application {
     def generate(namespace: Namespace): Unit = {
       currentPath = new File(currentPath, namespace.name)
       currentPath.mkdir()
-      //println("Path start "+currentPath+" ")
-
       currentPackage = currentPackage :+ namespace.name
-      //  println("Package start "+currentPackage+" ")
       namespace.namespaces().foreach { generate }
       namespace.structures().foreach { generate }
-
       currentPath = currentPath.getParentFile
-      //    println("Path end "+currentPath+" ")
       currentPackage = currentPackage diff List(namespace.name)
-      //      println("Package end "+currentPackage+" ")
+    }
+
+    // PascalCase
+    def toPascal(s: String): String = s.split("_").map(_.capitalize).mkString("")
+
+    // camelCase
+    def toCamel(s: String): String =
+      if (s.length > 0)
+        toPascal(s).substring(0, 1).toLowerCase + toPascal(s).substring(1)
+      else
+        s
+
+    def toStandartJavaType(datatype: String): String = {
+      datatype match {
+        case "int" => "int"
+        case "long" => "long"
+        case "bool" => "boolean"
+        case "string" => "String"
+        case "date" => "Date"
+        case "float" => "double"
+        case t => throw new IllegalArgumentException(s"Invalid DSL type: $t")
+      }
+    }
+
+    var writer: PrintWriter = null
+    def source(name: String)(body: => Unit): Unit = {
+      writer = new PrintWriter(new File(currentPath + toPascal(name) + ".java"))
+      body
+      writer.close
+    }
+
+    def block(str: String)(body: => Unit): Unit = {
+      writer.write(str)
+      writer.write("\n")
+      body
+    }
+
+    def ln(str: String): Unit = {
+      writer.write(str)
+      writer.write("\n")
     }
 
     def generate(structure: Structure): Unit = {
-      val writer = new PrintWriter(new File(currentPath, (structure.name + ".java")))
-      writer.write(s"package ${currentPackage.mkString(".")};\n")
-      writer.write(s"\npublic class " + structure.name.capitalize + " {\n")
-      for (f <- structure.fields()) {
-        writer.write("      private " + f.datatype + " " + f.name.toString() + ";\n")
+      source(structure.name) {
+        ln(s"package ${currentPackage.mkString(".")};")
+        val structName = toPascal(structure.name)
+        block(s"\npublic class " + structName + " {\n") {
+          structure.fields foreach { f =>
+            var fieldName = toCamel(f.name)
+            ln(" private " + toStandartJavaType(f.datatype) + " " + fieldName + ";")
+          }
+          for (f <- structure.fields()) {
+            var fieldName = toCamel(f.name)
+            ln(s"\n public void set$structName (" + toStandartJavaType(f.datatype) + s" $fieldName){")
+            ln(s"   this.$fieldName = $fieldName;\n }")
+          }
+          for (f <- structure.fields()) {
+            var fieldName = toCamel(f.name)
+            var fieldType = toStandartJavaType(f.datatype)
+            ln(s"\n public $fieldType get$structName (" + fieldType + s" $fieldName){")
+            ln(s"   return $fieldName;\n }")
+          }
+          ln("\n}")
+        }
       }
-      writer.close()
     }
   }
 }
