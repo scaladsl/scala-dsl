b27811c2b87d9dc0ab4ed68bc89126b7f29d7d40
anahitm <anahit.mirzoyan@iunetworks.am>
2017-03-03 16:47:25 +0400

    Blog test gen

---
+++ dsl-output/SparkArticleService.java
@@ -0,0 +1,77 @@
+import static spark.Spark.*;
+import spark.template.velocity.VelocityTemplateEngine;
+import spark.ModelAndView;
+import java.lang.Throwable;
+import java.util.*;
+import javax.persistence.EntityNotFoundException;
+import com.google.gson.Gson;
+import model.*;
+
+public class SparkArticleService{
+  public void includeArticleService(){
+    get("/api/articles/:id", (req, res) -> {
+    String jsonInString = "";
+    try{
+      model.Article articleService = new ArticleServiceImpl().retrieveById(UUID.fromString(req.params(":id")));
+      jsonInString = new Gson().toJson(articleService);
+    }
+    catch (EntityNotFoundException e){
+      halt(404, "Not Found");
+    }
+    catch (Throwable e){
+      halt(500, "Internal Server Error");
+    }
+    return jsonInString;
+    });
+    get("/api/articles", (req, res) -> {
+    String jsonInString = "";
+    try{
+      java.util.List<model.Article> articleServiceList = new ArticleServiceImpl().retrieveAll();
+      jsonInString = new Gson().toJson(articleServiceList);
+    }
+    catch (EntityNotFoundException e){
+      halt(404, "Not Found");
+    }
+    catch (Throwable e){
+      halt(500, "Internal Server Error");
+    }
+    return jsonInString;
+    });
+    post("/api/articles", (req, res) -> {
+    try{
+      new ArticleServiceImpl().add(ArticleFromJson.getArticle(req.body()));
+    }
+    catch (EntityNotFoundException e){
+      halt(404, "Not Found");
+    }
+    catch (Throwable e){
+      halt(500, "Internal Server Error");
+    }
+    return "";
+    });
+    put("/api/articles/:id", (req, res) -> {
+    try{
+      new ArticleServiceImpl().save(UUID.fromString(req.params(":id")), ArticleFromJson.getArticle(req.body()));
+    }
+    catch (EntityNotFoundException e){
+      halt(404, "Not Found");
+    }
+    catch (Throwable e){
+      halt(500, "Internal Server Error");
+    }
+    return "";
+    });
+    delete("/api/articles/:id", (req, res) -> {
+    try{
+      new ArticleServiceImpl().remove(UUID.fromString(req.params(":id")));
+    }
+    catch (EntityNotFoundException e){
+      halt(404, "Not Found");
+    }
+    catch (Throwable e){
+      halt(500, "Internal Server Error");
+    }
+    return "";
+    });
+  }
+}
